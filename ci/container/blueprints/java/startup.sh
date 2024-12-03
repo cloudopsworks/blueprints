649a79b6676edd9c0307297fe7130372d20e7a68
Cristian Beraha <cristian@cloudops.works>
2024-12-03 13:34:00 -0300

    chore: fix dockerfile for Java + unlock workflow

--- ci/container/blueprints/java/startup.sh
+++ ci/container/blueprints/java/startup.sh
@@ -2,4 +2,4 @@
 
 # Start the nodejs server in foreground
 cd /app/webapp
-$JAVA_HOME/bin/java -XX:MaxRAMPercentage=65 $JAVA_OPTS $APM_OPTS -jar /app/webapp/app.jar
+$JAVA_HOME/bin/java $JAVA_OPTS $APM_OPTS -jar /app/webapp/app.jar
